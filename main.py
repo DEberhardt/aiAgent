adb8b390afb33c31e83a1fa2ada5435ef80eb227
david.eberhardt@outlook.com <david.eberhardt@outlook.com>
2025-06-27 13:10:30 +0100

    adding messages list for prompt
    Signed-off-by: david.eberhardt@outlook.com <david.eberhardt@outlook.com>

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os, sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -13,11 +14,18 @@ question = sys.argv[1] if len(sys.argv) > 1 else None
 if not question:
     sys.exit(1)
 
+# We should have a question to ask now
+messages = [
+    types.Content(role="user", parts=[types.Part(text=question)]),
+]
+
 response = client.models.generate_content(
     model="gemini-2.0-flash-001",
-    contents=question
+    contents=messages
 )
 print(response.text)
 
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+if "--verbose" in sys.argv:
+    print(f"User prompt: {question}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
