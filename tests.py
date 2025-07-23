3381dfe0852613c0d71375abc1327b5606cc0ae4
david.eberhardt@outlook.com <david.eberhardt@outlook.com>
2025-07-23 14:25:11 +0100

    CH2_functions05
    Signed-off-by: david.eberhardt@outlook.com <david.eberhardt@outlook.com>

--- tests.py
+++ tests.py
@@ -1,10 +1,8 @@
 # When executed directly, it should run the get_files_info with the following paramaters.
 import os
-from functions.get_files_info import get_files_info
-from functions.get_file_content import get_file_content
-from functions.write_file import write_file
 
 # Testing get_files_info function with various cases
+from functions.get_files_info import get_files_info
 
 # # Should not raise and should return a list
 # get_files_info("calculator", ".")
@@ -20,6 +18,7 @@ from functions.write_file import write_file
 
 
 # Testing get_file_content function with various cases
+from functions.get_file_content import get_file_content
 
 # # Testing get_file_content function with truncation
 # get_file_content("calculator", "lorem.txt")
@@ -38,12 +37,36 @@ from functions.write_file import write_file
 
 
 # Testing write_file function with various cases
+from functions.write_file import write_file
 
-# Should overwrite the content of the existing file
-write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
+# # Should overwrite the content of the existing file
+# write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
 
-# Should create a file and write this text to it
-write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
+# # Should create a file and write this text to it
+# write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
 
-# Should fail because file does not exist
-write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
+# # Should fail because file does not exist
+# write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
+
+
+# Testing run_python_file function with various cases
+from functions.run_python_file import run_python_file
+
+# # This should print the calculator's usage instructions
+# run_python_file("calculator", "main.py")
+
+# # Should run the calculator with a different working directory
+# # (should run the calculator... which gives a kinda nasty rendered result)
+# run_python_file("calculator", "main.py", ["3 + 5"])
+
+# # This should run the calculator tests
+# run_python_file("calculator", "tests.py")
+
+# # This should return an error string
+# run_python_file("calculator", "../main.py")
+
+# # This should return an error string
+# run_python_file("calculator", "nonexistent.py")
+
+# # This should return an error string
+# run_python_file("calculator", "lorem.txt")
