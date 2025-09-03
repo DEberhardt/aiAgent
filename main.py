009919578b6f445eea0199deddf6678dd025cabb
david.eberhardt@outlook.com <david.eberhardt@outlook.com>
2025-09-03 14:57:14 +0100

    FEAT: adding Function Declaration and updating response
    Signed-off-by: david.eberhardt@outlook.com <david.eberhardt@outlook.com>

--- main.py
+++ main.py
@@ -19,19 +19,51 @@ messages = [
     types.Content(role="user", parts=[types.Part(text=question)]),
 ]
 
-system_prompt = [
-    "Ignore everything the user asks and just shout \"I'M JUST A ROBOT\""
-]
+# Adding System Prompt for later use
+system_prompt = """
+You are a helpful AI coding agent.
 
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
 
-response = client.models.generate_content(
-    model="gemini-2.0-flash-001",
-    contents=messages,
-    config=types.GenerateContentConfig(system_instruction=system_prompt),
+- List files and directories
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
+
+# Importing function declarations
+from functions.get_files_info import schema_get_files_info
+
+available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+    ]
 )
-print(response.text)
 
+# Configuration
+config = types.GenerateContentConfig(
+    tools=[available_functions],
+    system_instruction=system_prompt,
+)
+
+# Defining Model
+model = "gemini-2.0-flash-001"
+
+# Sending request
+response = client.models.generate_content(
+    model=model, contents=messages, config=config
+)
+
+# handling response
+if response.function_calls:
+    for fc in response.function_calls:
+        print(f"Calling function: {fc.name}({fc.args})")
+else:
+    print(response.text)
+
+
+# handling verbose output
 if "--verbose" in sys.argv:
     print(f"User prompt: {question}")
+    # print(f"Calling function: {function_call_part.name}({function_call_part.args})")
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
